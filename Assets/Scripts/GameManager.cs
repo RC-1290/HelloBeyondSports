cfd421f8204bdd31bbe9ba37c6e272e9b3618a7a
Laurens Mathot <laurens.mathot@codeanimo.com>
2020-08-21 17:23:12 +0200

    Added spawning of a bunch of prefabs that get their position from the parser. Added an amazing field with some reference points that I might update once I figure out what game this is.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿// Copyright Code Animo (Laurens Mathot) 2020, All rights reserved. Example project for Beyond Sports (https://www.beyondsports.nl),
+using UnityEngine;
+
+using System.Collections.Generic;
+
+namespace CodeAnimo
+{
+	public class GameManager : MonoBehaviour
+	{
+		public Parser parser;
+		public GameObject TrackedObjectPrefab;
+		[Tooltip("Desired TrackedObject Count. Only updated OnEnable.")]
+		public int TrackedObjectCount = 30;
+
+		private List<TrackedObject> _trackedObjects;
+
+		private void OnEnable()
+		{
+			_trackedObjects = new List<TrackedObject>(TrackedObjectCount);
+			for (int trackedObjectIndex = 0; trackedObjectIndex < TrackedObjectCount; ++ trackedObjectIndex)
+			{
+				GameObject createdObject = Instantiate(TrackedObjectPrefab);
+				TrackedObject trackedObject = createdObject.GetComponent<TrackedObject>();
+
+				if (trackedObject == null)
+				{
+					Debug.LogError("The given TrackedObjectPrefab does not contain a TrackedObject components in the root.", this);
+				}
+				trackedObject.Index = trackedObjectIndex;
+				trackedObject.Parser = parser;
+
+				_trackedObjects.Add(trackedObject);
+
+			}
+
+
+
+
+		}
+		private void OnDisable()
+		{
+			for (int trackedObjectIndex = 0; trackedObjectIndex < TrackedObjectCount; ++ trackedObjectIndex)
+			{
+				TrackedObject trackedObject = _trackedObjects[trackedObjectIndex];
+				Destroy(trackedObject.gameObject);
+			}
+			_trackedObjects.Clear();
+			_trackedObjects = null;
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/TrackedObject.cs
@@ -0,0 +1,36 @@
+﻿// Copyright Code Animo (Laurens Mathot) 2020, All rights reserved. Example project for Beyond Sports (https://www.beyondsports.nl),
+using UnityEngine;
+
+namespace CodeAnimo
+{
+	public class TrackedObject : MonoBehaviour
+	{
+		public Parser Parser;
+		public int Index = 0;
+		public int TeamNumber;
+		public int ShirtNumber;
+
+		private Transform _transform;
+
+		private void OnEnable()
+		{
+			_transform = GetComponent<Transform>();
+		}
+
+		private void LateUpdate()
+		{
+			// Remember that ECS stuff? Yeah, whatever, first version is just gonna ruin the cache for now ;)
+			TeamNumber			= Parser.TeamNumbers[Index];
+			ShirtNumber			= Parser.ShirtNumbers[Index];
+			int xCentimeters	= Parser.CentimetersX[Index];
+			int yCentimeters	= Parser.CentimetersY[Index];
+			float speed			= Parser.Speeds[Index];
+
+			Vector3 newPosition = new Vector3(xCentimeters / 100.0f, 0, yCentimeters / 100.0f);
+
+			_transform.position = newPosition;
+
+		}
+
+	}
+}
