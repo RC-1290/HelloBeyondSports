6b5b500352e7fe31de0c46c4083b798daab09515
Laurens Mathot <laurens.mathot@codeanimo.com>
2020-08-24 18:37:28 +0200

    Added tracked ball.

---
+++ Assets/Scripts/TrackedBall.cs
@@ -0,0 +1,31 @@
+﻿// Copyright Code Animo (Laurens Mathot) 2020, All rights reserved. Example project for Beyond Sports (https://www.beyondsports.nl),
+using UnityEngine;
+
+namespace CodeAnimo
+{
+	public class TrackedBall : MonoBehaviour
+	{
+		public Parser Parser;
+		
+		private Transform _transform;
+		private float _centimetersToMeters = .01f;
+
+		private void OnEnable()
+		{
+			_transform = GetComponent<Transform>();
+		}
+
+		private void LateUpdate()
+		{
+			int xCentimeters	= Parser.BallX;
+			int yCentimeters	= Parser.BallY;
+			int zCentimeters	= Parser.BallZ;
+			float speed			= Parser.BallSpeed;
+
+			Vector3 newPosition = new Vector3(xCentimeters * _centimetersToMeters, zCentimeters * _centimetersToMeters, yCentimeters * _centimetersToMeters);
+
+			_transform.position = newPosition;
+		}
+
+	}
+}
