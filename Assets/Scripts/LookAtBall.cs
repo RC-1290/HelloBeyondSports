f2509fe32710e79899443e8049e648731a65e0c9
Laurens Mathot <laurens.mathot@codeanimo.com>
2020-08-24 18:55:50 +0200

    Improved visuals a bit. Added a camera that looks at the ball.

---
+++ Assets/Scripts/LookAtBall.cs
@@ -0,0 +1,23 @@
+﻿// Copyright Code Animo (Laurens Mathot) 2020, All rights reserved. Example project for Beyond Sports (https://www.beyondsports.nl),
+using UnityEngine;
+
+namespace CodeAnimo
+{
+	public class LookAtBall : MonoBehaviour
+	{
+		public Transform Ball;
+
+		private Transform _transform;
+
+
+		private void OnEnable()
+		{
+			_transform = GetComponent<Transform>();
+		}
+
+		private void Update()
+		{
+			_transform.LookAt(Ball, Vector3.up);
+		}
+	}
+}
