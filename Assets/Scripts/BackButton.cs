6a8e979b75221dd0c828f2ca486c0f2748d0d36b
Laurens Mathot <laurens.mathot@codeanimo.com>
2020-08-25 17:11:02 +0200

    Set Build Settings for Android

---
+++ Assets/Scripts/BackButton.cs
@@ -0,0 +1,13 @@
+﻿// Copyright Code Animo (Laurens Mathot) 2020, All rights reserved. Example project for Beyond Sports (https://www.beyondsports.nl),
+using UnityEngine;
+
+namespace CodeAnimo
+{
+	public class BackButton : MonoBehaviour
+	{
+		private void OnEnable()
+		{
+			Input.backButtonLeavesApp = true;
+		}
+	}
+}
